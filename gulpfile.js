db2c5f961f7444a1a23667b24b2092db3a5d3703
John Unruh <j_unruh@hotmail.com>
2015-06-16 13:42:02 -0600

    utils unit tests pass

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
 var streamify = require('gulp-streamify');
+var babelify = require('babelify');
+var jasmine = require('jasmine');
+var reporters = require('jasmine-reporters');
+var combiner = require('stream-combiner2');
 
 var path = {
     HTML: './src/index.html',
@@ -14,7 +18,12 @@ var path = {
     DEST: './index.html',
     DEST_BUILD: './js',
     DEST_SRC: './js',
-    ENTRY_POINT: './src/js/app.js'
+    ENTRY_POINT: './src/js/client/app.js',
+    CLIENT_JS: ['scr/js/client/**', 'src/js/common/**', 'src/js/components/**', 'src/js/dispatchers/**', 'src/js/stores/**'],
+    CLIENT_JASMINE: ['src/js/client/**', 'src/js/common/**', 'src/js/dispatchers/**', 'src/js/stores/**'],
+    SERVER_JS: ['scr/js/server/**', 'src/js/common/**'],
+    CLIENT_OUT: 'app.js',
+    SERVER_OUT: 'server.js'
 };
 
 gulp.task('copy', function(){
@@ -27,12 +36,12 @@ gulp.task('watch', function() {
 
     var watcher  = watchify(browserify({
         entries: [path.ENTRY_POINT],
-        transform: [reactify],
+        transform: [babelify],
         debug: true,
         cache: {}, packageCache: {}, fullPaths: true
     }));
 
-    return watcher.on('update', function () {
+    var combined = combiner.obj([ watcher.on('update', function () {
         watcher.bundle()
             .pipe(source(path.OUT))
             .pipe(gulp.dest(path.DEST_SRC))
@@ -40,13 +49,41 @@ gulp.task('watch', function() {
     })
         .bundle()
         .pipe(source(path.OUT))
-        .pipe(gulp.dest(path.DEST_SRC));
+        .pipe(gulp.dest(path.DEST_SRC))
+    ]);
+    // any errors in the above streams will get caught
+    // by this listener, instead of being thrown:
+    combined.on('error', console.error.bind(console));
+
+    return combined;
+
 });
+
+var allSpecFiles = function(dir) {
+    if(!fs.statSync(dir).isDirectory())
+        throw("must be a directory");
+    // all directory entries
+    var dirContent = fs.readdirSync(dir).map(function(name) { return dir + '/' + name });
+    // only files ending is 'spec.js'
+    var files = dirContent.filter(function(fname) { return (fs.statSync(fname).isFile() && (fname.search('spec.js$') != -1))});
+    // all directories
+    var directories = dirContent.filter(function(fname) { return fs.statSync(fname).isDirectory()});
+
+    // if no subdirectories simply return array of files
+    if(directories.length <= 0)
+        return files;
+    // return array of files concatenated with the files found in all subdirectories (and their subdirectories)
+    // the ruduce is just a way to flatten a two level array, since js array doesnt have flatter() or flatmap()
+    return files.concat(directories.map(function(dirname) { return allSpecFiles(dirname) }).reduce(function(acc, fileArray) {
+        return acc.concat(fileArray);
+    }))
+};
+
 
 gulp.task('build', function(){
     browserify({
         entries: [path.ENTRY_POINT],
-        transform: [reactify],
+        transform: [babelify],
     })
         .bundle()
         .pipe(source(path.MINIFIED_OUT))
--- spec/utils-spec.js
+++ spec/utils-spec.js
@@ -1,15 +1,16 @@
-utils = require('../src/js/utils');
+utils = require('../src/js/common/utils.js');
 
 describe("determine if a player is already scheduled for a tee time", function () {
-    it("can determine if a player is currently not on the tee list", function () {
-        var db = [{time: "10:36", position: 0, players: ["Joe", "Billy Bob", "Spike", "Karen"], maxPlayers: 4},
-            {time: "10:42", position: 1, players: ["Annie", "Tommy"], maxPlayers: 4},
-            {time: "10:48", position: 2, players: [], maxPlayers: 2}];
+    var db = {flights: [{time: "10:36", position: 0, players: ["Joe", "Billy Bob", "Spike", "Karen"], maxPlayers: 4},
+        {time: "10:42", position: 1, players: ["Annie", "Tommy"], maxPlayers: 4},
+        {time: "10:48", position: 2, players: [], maxPlayers: 2}]};
 
-        expect(utils.inTeeList(db, "Harry").toBe(false));
+    it("can determine if a player is currently not on the tee list", function () {
+
+        expect(utils.inTeeList(db, "Harry")).toBe(false);
     });
     it("can determine that a player is on the current tee time list", function(){
-        expect(utils.inTeeList(db, "Billy Bob").toBe(true));
+        expect(utils.inTeeList(db, "Billy Bob")).toBe(true);
     });
 });
 
@@ -19,35 +20,34 @@ describe("find the flights with availability", function() {
             {time: "10:42", position: 1, players: ["Annie", "Tommy"], maxPlayers: 4},
             {time: "10:48", position: 2, players: [], maxPlayers: 2}];
 
-        expect(utils.availableFlights(db).toEqual([{time: "10:42", position: 1, players: ["Annie", "Tommy"], maxPlayers: 4},
-            {time: "10:48", position: 2, players: [], maxPlayers: 2}]
-        ))
+        expect(utils.availableFlights(db)[0].time).toEqual("10:42");
+        expect(utils.availableFlights(db)[1].time).toEqual("10:48");
     });
 
     it("returns an empty array when all flights are full", function() {
         var db = [{time: "10:36", position: 0, players: ["Joe", "Billy Bob", "Spike", "Karen"], maxPlayers: 4},
             {time: "10:42", position: 1, players: ["Annie", "Tommy"], maxPlayers: 2},
             {time: "10:48", position: 2, players: ["Ben", "Tammy"], maxPlayers: 2}];
-        expect(utils.availableFilghts(db).toEqual([]));
+        expect(utils.availableFlights(db)).toEqual([]);
     });
 
     it("returns an empty array when the db is empty", function() {
-        expect(utils.availableFlights([]).toEqual([]))
+        expect(utils.availableFlights([])).toEqual([]);
     });
 });
 
 describe("determine if a flight is full", function () {
-    it("can determine if a full flight is full", function () {
-        var db = [{time: "10:36", position: 0, players: ["Joe", "Billy Bob", "Spike", "Karen"], maxPlayers: 4},
-            {time: "10:42", position: 1, players: ["Annie", "Tommy"], maxPlayers: 4},
-            {time: "10:48", position: 2, players: [], maxPlayers: 2}];
+    var db = [{time: "10:36", position: 0, players: ["Joe", "Billy Bob", "Spike", "Karen"], maxPlayers: 4},
+        {time: "10:42", position: 1,players: ["Annie", "Tommy"], maxPlayers: 4},
+        {time: "10:48", position: 2, players: [], maxPlayers: 2}];
 
-        expect(flightFull(db[0]).toBe(true));
+    it("can determine if a full flight is full", function () {
+        expect(utils.flightFull(db[0])).toBe(true);
     });
     it("can determine that a flight that is not full is not full", function () {
-        expect(utils.filghtFull(db[1]).toBe(false));
+        expect(utils.flightFull(db[1])).toBe(false);
     });
     it("can determine that an empty flight is not full", function () {
-        expect(utils.flightFull(db[2]).toBe(false));
+        expect(utils.flightFull(db[2])).toBe(false);
     });
 });
